72d9b42bebe202bb0c5bd5333286b025859529c8
Sven Neumann <s.neumann@websix.de>
2017-04-15 17:31:58 +0200

    Set Query Builder public

--- src/themazim/Cassandra/Query/Builder.php
+++ src/themazim/Cassandra/Query/Builder.php
@@ -55,7 +55,7 @@ class Builder extends BaseBuilder
      *
      * @var array
      */
-    protected $operators = [
+    public $operators = [
         '=', '<', '>', '<=', '>=', '<>', '!=',
         'like', 'not like', 'between', 'ilike',
         '&', '|', '^', '<<', '>>',
